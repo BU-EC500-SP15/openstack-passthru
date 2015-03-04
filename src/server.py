6d4ae5ac22c537c3c61bfb0a4917dc14c11cdec3
JasjotSingh <jasjot_jrarjn1@yahoo.in>
2015-03-04 18:29:46 -0500

    changed create and also implemented a prototype for delete container.

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 import os
 import socket
-import swiftclient
+from swiftclient import client
 #from flask import Flask, request, redirect, url_for,send_from_directory
 #from werkzeug import secure_filename
 usr = 'admin'
@@ -10,26 +10,41 @@ ten = 'admin'
 
 container_name = 'my-new-container'
 
-def connect_swift():
-	conn = swiftclient.Connection('http://10.0.2.15:35357/v2.0','admin', 'open',auth_version='2',tenant_name= 'admin', retries=5,)
-	if(conn.put_container('my-cont1')):
-		conn.close()
-		return 1
+def connect_swift():			###for createing connection to the account and getting an object to work on swift.
+	con = client.Connection(authurl=authurl,user=usr, key=key,auth_version='2',tenant_name= 'admin', retries=5,)
+	return con
+	
+def create_container(con):	###for creating a container from the connection object.
+	try:
+		con.put_container('a1')
+	except:
+		print 'error'
+		return
 	else:
-		conn.close()
-		return 0
+		print 'OK'
+		return
+def delete_container(con):	### for deleting a container from the connection object.
+	try:
+		con.delete_container('a1')
+	except:
+		print 'error'
+		return
+	else:
+		print 'OK'
+		return
 
 if __name__ == '__main__':
 	
 	s = socket.socket()
 	host = socket.gethostname()
 	port = 5002
-	s.bind((host,port))
-	s.listen(5)
-	c, addr = s.accept()
+	s.bind((host,port))		##binding port and host to socket.
+	s.listen(5)			###waiting for the client connection.
+	c, addr = s.accept()		###accepting the client connection.
 	st =  ''
 	c.send('Welcome to test_server:\n')	
 	c.send('>>>')
+	conn = connect_swift()		###getting a connection object.
 	while (True):
 		
 		print 'Got connection from ',addr
@@ -38,14 +53,13 @@ if __name__ == '__main__':
 		if st =='exit':
 			break
 		elif st == 'post':
-			conn = connect_swift()
-			if(conn == 1):
-				print 1
-			else:
-				print 0
+			create_container(conn)
+		elif st == 'delete':
+			delete_container(conn)
+			
 		#print 'send ....'
 		#c.send('>>>')
 		
-		
-	c.close()
+	conn.close()		###closing the swift connection.
+	c.close()		###closing the client connection.
 

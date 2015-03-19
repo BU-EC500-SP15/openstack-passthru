d3e7e8bbf0962cab84736a5dd22f984e6b1031ef
jasjotsingh <jasjot_jrarjn1@yahoo.in>
2015-03-18 21:48:42 -0400

    updated server, uses flask to make a server proxy. tested using CURL command rather than client communcating via socket connection.

--- src/server.py
+++ src/server.py
@@ -1,65 +1,56 @@
 import os
 import socket
 from swiftclient import client
-#from flask import Flask, request, redirect, url_for,send_from_directory
-#from werkzeug import secure_filename
-usr = 'admin'
-key = 'open'
-authurl = 'http://10.0.2.15:35357/v2.0'
-ten = 'admin'
+usr = 'george'
+key = '[passwordhere]'
+authurl = 'http://140.247.152.207:35357/v2.0'
+ten = 'EC500-openstack-passthru'
 
-container_name = 'my-new-container'
 
 def connect_swift():			###for createing connection to the account and getting an object to work on swift.
-	con = client.Connection(authurl=authurl,user=usr, key=key,auth_version='2',tenant_name= 'admin', retries=5,)
+	con = client.Connection(authurl=authurl,user=usr, key=key,auth_version='2',tenant_name=ten, retries=5,)
 	return con
-	
-def create_container(con):	###for creating a container from the connection object.
-	try:
-		con.put_container('a1')
-	except:
-		print 'error'
-		return
-	else:
-		print 'OK'
-		return
-def delete_container(con):	### for deleting a container from the connection object.
-	try:
-		con.delete_container('a1')
-	except:
-		print 'error'
-		return
-	else:
-		print 'OK'
-		return
 
-if __name__ == '__main__':
-	
-	s = socket.socket()
-	host = socket.gethostname()
-	port = 5002
-	s.bind((host,port))		##binding port and host to socket.
-	s.listen(5)			###waiting for the client connection.
-	c, addr = s.accept()		###accepting the client connection.
-	st =  ''
-	c.send('Welcome to test_server:\n')	
-	c.send('>>>')
-	conn = connect_swift()		###getting a connection object.
-	while (True):
-		
-		print 'Got connection from ',addr
-		st = c.recv(1024)
-		print st
-		if st =='exit':
-			break
-		elif st == 'post':
-			create_container(conn)
-		elif st == 'delete':
-			delete_container(conn)
-			
-		#print 'send ....'
-		#c.send('>>>')
-		
-	conn.close()		###closing the swift connection.
-	c.close()		###closing the client connection.
+from swiftclient import ClientException
 
+def create_container(con, container):	###for creating a container from the connection object.
+	try:
+		con.put_container(container)
+	except ClientException as e:
+                return e.msg, e.http_status
+	else:
+		return "", 204
+def delete_container(con, container):	### for deleting a container from the connection object.
+	try:
+		con.delete_container(container)
+	except ClientException as e:
+		return e.msg, e.http_status
+	else:
+                return "", 204
+
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/<container>", methods=['PUT', 'DELETE', 'GET', 'POST', 'HEAD'])
+def hello(container):
+        if request.method == 'PUT':
+                con = connect_swift()
+                return create_container(con, container)
+        elif request.method == 'DELETE':
+                con = connect_swift()
+                return delete_container(con, container)
+        else:
+                return "Not yet implemented", 501
+
+
+@app.route("/<container>/<obj>", methods=['PUT', 'DELETE', 'GET', 'POST', 'HEAD'])
+def func2(container, obj):
+        return "Not Yet Implemented", 501
+
+@app.route("/", methods=['GET'])
+def bluh():
+        return "", 204
+
+if __name__ == "__main__":
+        app.run()
